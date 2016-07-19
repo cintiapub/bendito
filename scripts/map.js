8d0b138842bf7a0ab1e6a170277ec162e4b772d3
Cintia Romero <cintiapub@gmail.com>
2016-07-18 18:50:30 -0700

    initial version

---
+++ scripts/map.js
@@ -0,0 +1,20 @@
+function init_map() {
+    var myOptions = {
+        zoom: 10,
+        center: new google.maps.LatLng(37.7652065, -122.24163550000003),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);
+    marker = new google.maps.Marker({
+        map: map,
+        position: new google.maps.LatLng(37.7652065, -122.24163550000003)
+    });
+    infowindow = new google.maps.InfoWindow({
+        content: '<strong>Bendito Food Truck</strong><br>Alameda, CA<br>'
+    });
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map, marker);
+    });
+    infowindow.open(map, marker);
+}
+google.maps.event.addDomListener(window, 'load', init_map);
